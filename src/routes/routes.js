c9abe4187d66ea20d3d432271f5a8c29284bb20a
David D <dsdiazs@falabella.cl>
2020-09-11 23:08:47 -0300

    add method delete

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,7 +5,6 @@ const base_path = '/api';
 module.exports = function ( app ){
     
     app.get(base_path+'/spotify/', function (req, res) {
-        console.log("route---> paso 1");
         try{
             spotifyController.getSpotifyController( req, res);
         }catch(e){
@@ -24,22 +23,13 @@ module.exports = function ( app ){
     
     app.post(base_path+'/spotify/', function (req, res) {
         try{
-            console.log( req.body );
-            
-            console.log("paso -1");
-            console.log(req.body);
-            console.log(req.body.typeSearch);
-            console.log(req.body.artist);
             if( req && req.body && req.body.typeSearch && req.body.artist ){
-                console.log("paso 0");
                 const req_body_spotify = {
                     "typeSearch": req.body.typeSearch,
                     "artist": req.body.artist
                 };
-                console.log("paso 1");
                 spotifyController.addSpotifyController( req, res, req_body_spotify );
             }else{
-                console.log("paso 2");
                 dataError = {
                     modules: 'routes.js',
                     method: 'app.post',
@@ -48,13 +38,21 @@ module.exports = function ( app ){
                 middlewareResponse.generateResponseError(req, res, dataError);
             }
         }catch(e){
-            console.log("paso 3");
             dataError = {
                 modules: 'routes.js',
                 method: 'app.post',
                 message: e.message
             }
             middlewareResponse.generateResponseError(req, res, dataError);
+        }
+    });
+
+    app.delete(base_path+'/spotify/:id', function (req, res) {
+        try{
+            spotify_id = req.params.id;
+            spotifyController.deleteSpotifyController(req, res, spotify_id);
+        }catch(e){
+            console.log(e);
         }
     });
 
